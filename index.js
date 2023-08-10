13b14214ecb42348d4afb7f0e904f7c66b03031a
maserekafelex <maserekafelex1@gmail.com>
2023-08-10 03:27:49 +0300

    Fix required changes

--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const speakersData = [
   {
     name: 'Yochai Benkler',
     title:
-      'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+      'Berkman Professor of Entrepreneurial ',
     bio: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks, in 2006',
-    image: 'Assets/feat img/yochai.JPG',
+    image: 'Assets/bg/image/spk1.PNG',
   },
   {
     name: 'SohYeong Noh',
-    title: 'Director of Art Centre Nabi and a board member of CC Korea',
+    title: 'Director of Art Centre Nabi ',
     bio: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science, technology, humanities, and the arts.',
-    image: 'Assets/feat img/noh.JPG',
+    image: 'Assets/bg/image/spk2.PNG',
   },
   {
     name: 'Lila Tretikov',
-    title: 'Executive Director of the Wikimedia Foundation',
-    bio: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikimedia, freely available in 290 languages and used by nearly half a billion people around the world every month.',
-    image: 'Assets/feat img/lila.JPG',
+    title: 'Executive Director of the Wikimedia',
+    bio: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikimedia, freely available in 290 languages and used by nearly half a billion people around the world.',
+    image: 'Assets/bg/image/spk3.PNG',
   },
   {
     name: 'Kilnam Chon',
-    title: 'Outspoken advocate for the open web and digital commons',
+    title: 'Outspoken advocate for the open web',
     bio: "Kilnam Chon helped bring the internet to Asia and was inducted into the inaugural class of the Internet Society's Internet Hall of Fame.",
-    image: 'Assets/feat img/kilnam chon.JPG',
+    image: 'Assets/bg/image/spk4.PNG',
   },
   {
     name: 'Julia Leda',
     title: 'President of Young Pirates of Europe',
     bio: 'European integration, political democracy, and youth participation through online platforms are her major focuses.',
-    image: 'Assets/feat img/julia leda.jpg',
+    image: 'Assets/bg/image/spk5.PNG',
   },
   {
     name: 'Ryan Merkley',
-    title: 'CEO of Creative Commons, ex COO of the Mozilla Foundation',
+    title: 'CEO of Creative Commons',
     bio: 'Ryan has been leading open-source projects at the Mozilla Foundation, such as the open-source movement.',
-    image: 'Assets/feat img/ryanmerkley.jpg',
+    image: 'Assets/bg/image/spk6.PNG',
   },
 ];
 
